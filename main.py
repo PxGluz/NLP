521f4c52bba18ec52c2d30d0a3e1d912c67bb855
Popa Bogdan-Gabriel <badddog25@gmail.com>
2022-03-26 12:04:33 +0200

    Gata stim (glumeam)

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import json
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    fisier = open("train.json")
+    data = json.load(fisier)
+    for i in data:
+        for j in i['ner_tags']:
+            print(j)
+        for j in i['ner_ids']:
+            print(j)
+        for j in i['tokens']:
+            print(j)
+        for j in i['space_after']:
+            print(j)
